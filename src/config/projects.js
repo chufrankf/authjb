dd5e6170e378bfa6a7b3e36178f20d880fa67f91
Frank Chu <chufrankf@gmail.com>
2019-01-28 23:45:30 -0800

    updating the projects page

---
+++ src/config/projects.js
@@ -0,0 +1,14 @@
+export default [
+  { 
+    title: 'AuthJB',
+    description: 'Authorization server and a frontdoor to all for applications on jibaga.com.',
+    github: 'https://github.com/chufrankf/authjb',
+    link: '/'
+  },
+  {
+    title: 'New Project',
+    description: 'My new project description here',
+    github: 'https://github.com/chufrankf',
+    link: '/'
+  }
+];
--- src/routers/frontdoor.js
+++ src/routers/frontdoor.js
@@ -1,6 +1,8 @@
+import projects from '../config/projects';
+
 export default function(app) {
   // Frontdoor Routers
-  app.get('/', (req, res) => res.render('home', {user: req.user}) );
+  app.get('/', (req, res) => res.render('home', {projects: projects, user: req.user}) );
   app.get('/login', (req, res) => res.render('auth/signin', {signup: false, user: req.user}) );
   app.get('/signup', (req, res) => res.render('auth/signin', {signup: true, user: req.user}) );
 }
